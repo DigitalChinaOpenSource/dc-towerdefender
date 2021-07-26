eca7056f40e624e49036dbb99035883cbb4a3e98
MajorHorse <mylove103@foxmail.com>
2021-07-26 15:30:58 +0800

    服务端静态路径定义

--- js/server.js
+++ js/server.js
@@ -2,16 +2,19 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var path = require('path');
 
 //项目根目录
-var path=require('path');
-var rootPath=path.join(__dirname,'../');
+app.use(express.static(path.join(__dirname, 'public')));
+//静态文件目录
+var staticPath = path.join(__dirname, 'public');
+// var rootPath=path.join(__dirname,'../');
 
 //数据库初始化配置
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: '10.3.70.132',
-    port:'30772',
+    port: '30772',
     user: 'root',
     password: 'ToNpxg699e',
     database: 'tower_defense_v4.0.1'
@@ -21,17 +24,17 @@ var urlEncodedParser = bodyParser.urlencoded({ extended: false })
 
 //首页重定向
 app.get('/index', function (req, res) {
-    res.sendFile(rootPath+'index.html');
+    res.sendFile(staticPath + '/ndex.html');
 })
 
 //登录重定向
 app.get('/login', function (req, res) {
-    res.sendFile(rootPath+'log.html');
+    res.sendFile(staticPath + '/log.html');
 })
 
 //注册重定向
 app.get('/regist', function (req, res) {
-    res.sendFile(rootPath+'log.html');
+    res.sendFile(staticPath + '/log.html');
 })
 
 //注册
@@ -39,10 +42,10 @@ app.post('/regist', urlEncodedParser, function (req, res) {
     var userName = req.body.username;
     var password = req.body.password;
     var params = [userName, password];
-    var sql='insert into users(name,password,score) values(?,?,0)';
+    var sql = 'insert into users(name,password,score) values(?,?,0)';
     connection.query(sql, params, function (error, result) {
-        if(error){
-            console.log('ERROR--'+error.message);
+        if (error) {
+            console.log('ERROR--' + error.message);
             return;
         }
         console.log('----------SELECT RESULT----------');
@@ -52,30 +55,30 @@ app.post('/regist', urlEncodedParser, function (req, res) {
 })
 
 //登录
-app.post('/login',urlEncodedParser,function(req,res){
+app.post('/login', urlEncodedParser, function (req, res) {
     var userName = req.body.username;
     var password = req.body.password;
     var params = [userName, password];
-    var sql='select * from users where name=? and password=?';
-    connection.query(sql,params,function(error,result){
-        if(error){
-            console.log('ERROR--'+error.message);
+    var sql = 'select * from users where name=? and password=?';
+    connection.query(sql, params, function (error, result) {
+        if (error) {
+            console.log('ERROR--' + error.message);
             return;
         }
         //登录失败
-        if(result.length==0){
+        if (result.length == 0) {
             console.log("用户名或者密码错误！");
-            res.sendFile(rootPath+'login_fail.html');
+            res.sendFile(staticPath + 'login_fail.html');
             return;
         }
         //登录成功
         //登录信息存入Session内，定位至首页（后续加）
-        res.sendFile(rootPath+'login_success.html');
+        res.sendFile(staticPath + 'login_success.html');
     })
 })
 
 //监听
-var server = app.listen(8081, function () {
+var server = app.listen(8082, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("访问地址为：http://%s:%s", host, port)
