08e66f8c991aba5cccd46ca90e66d8cf06c72217
yinxbc <yinxbc@digitalchina.com>
2021-07-27 16:12:52 +0800

    增加防御塔发射子弹的逻辑

--- public/js/Tower.js
+++ public/js/Tower.js
@@ -1,14 +1,16 @@
 class Tower {
     //塔的范围区分塔的种类
-    constructor(x, y, type) {
+    constructor(x, y, type,range,attack_interval,cost,sale) {
         this.x = x;
         this.y = y;
         this.tower_level = 1;
-        this.type = type || {};
-        this.range = this.type.range ;
-        this.tower_img = this.type.tower_img;
-        this.cost = this.type.cost; //建塔所需金币
-        this.sale = this.type.sale; //卖塔所得金币   
+        this.type = type ;
+        this.range = range ;
+        this.attack_interval = attack_interval;
+        this.tower_img = tower_img;
+        this.cost = cost; //建塔所需金币
+        this.sale = sale; //卖塔所得金币 
+        this.check_attack_interval = undefined;  
     }
 }
 
