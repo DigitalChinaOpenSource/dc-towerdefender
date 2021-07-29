d8e446afdaf2966f36f7820490231668ce5e852d
ZHUZQC <zhuzqc@digitalchina.com>
2021-07-29 13:28:36 +0800

    请求合并

--- public/js/Player.js
+++ public/js/Player.js
@@ -1,7 +1,25 @@
 class Player {
-    constructor() {
-        this.money = PlAYER_BASE_MONEY + (LEVEL * 100);
-        this.end_hp = END_HP; //基地血量
+    constructor(nickName,score) {
+        this.nickname = nickName; //数据库昵称字段
+        this.score = score; //数据库积分字段
 
     }
+        //set方法
+        setNickname(nickName) {
+            this.nickname=nickName;
+        }
+        setScore(score) {
+            this.score=score;
+        }    
+
+        //get方法
+        getNickame() {
+            return this.nickName;
+        }
+        getScore() {
+            return this.score;
+        }  
+
+        // this.money = PlAYER_BASE_MONEY + (LEVEL * 100); //老版本基地血量
+        // this.end_hp = END_HP; //基地血量
 }
